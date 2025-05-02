a18c333058fd59e24b2004ff848a6e4e9c4e6a90
SharkGuo <guo012599@gmail.com>
2025-05-03 04:56:08 +0800

    Update player_death.cs

--- Assets/Shark/Script/player_death.cs
+++ Assets/Shark/Script/player_death.cs
@@ -11,37 +11,33 @@ public class player_death : MonoBehaviour
     private bool isDead = false; // 防止多次触发死亡
 
 
-
+    /*
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 可以使用标签检测敌人，也可以在敌人身上添加特定组件检测
         if (!isDead && collision.gameObject.CompareTag("Enemy"))
         {
-            if (DeathMarker2D.Instance != null)
-            {
-                DeathMarker2D.Instance.RecordDeathPosition(playerCharacter.transform.position);
-            }
             Die();
         }
     }
 
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 同上，适用于碰撞体而非触发器的情况
         if (!isDead && collision.gameObject.CompareTag("Enemy"))
         {
-            if (DeathMarker2D.Instance != null)
-            {
-                DeathMarker2D.Instance.RecordDeathPosition(playerCharacter.transform.position);
-            }
             Die();
         }
     }
+    */
 
 
-    private void Die()
+    public void Die()
     {
+        if (DeathMarker2D.Instance != null){
+            DeathMarker2D.Instance.RecordDeathPosition(playerCharacter.transform.position);
+        }
         isDead = true;
         // 延迟后重置场景
         Invoke("ResetScene", resetDelay);
