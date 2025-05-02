5a6118e3ded639701b7fedee65fb531e28d68e4d
dabu0319 <407790750@qq.com>
2025-05-02 01:34:52 -0400

    Indicator

---
+++ Assets/Dabu/Scripts/MouseDirIndicator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MouseDirIndicator : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mouseScreenPos = Input.mousePosition;
+        Vector3 lineScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = mouseScreenPos - lineScreenPos;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);  // 因为初始是向上
+    }
+}
