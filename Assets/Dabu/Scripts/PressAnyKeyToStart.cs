0e1760f2393589f2213cd357fe2e51d18e259f9c
dabu0319 <407790750@qq.com>
2025-05-03 22:29:31 -0400

    StartScene

---
+++ Assets/Dabu/Scripts/PressAnyKeyToStart.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.Events;
+
+public class PressAnyKeyController : MonoBehaviour
+{
+    public TextMeshProUGUI pressText;
+
+    private bool hasPressedKey = false;
+    private Tween blinkTween;
+
+    public UnityEvent onStart;
+
+    void Start()
+    {
+        // 初始透明度设置为1
+        pressText.alpha = 1f;
+
+        // 闪烁动画：无限淡入淡出
+        blinkTween = pressText.DOFade(0f, 1f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutSine);
+    }
+
+    void Update()
+    {
+        if (!hasPressedKey && Input.anyKeyDown)
+        {
+            hasPressedKey = true;
+
+            // 停止闪烁
+            blinkTween.Kill();
+
+            // 淡出当前文字，然后显示“1”
+            pressText.DOFade(1f, 0.5f).OnComplete(() =>
+            {
+                DOVirtual.DelayedCall(0.5f, () =>
+                {
+                    pressText.DOFade(0f, 0.5f);
+                        
+                    // 调用事件
+                    onStart?.Invoke();
+                });
+            });
+        }
+    }
+}
